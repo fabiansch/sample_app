e281437fd4b797f66777aab3d9c28f42fccf19f7
fabian <stahlschiff@web.de>
2017-07-21 23:54:19 +0200

    finish user index and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,15 +6,45 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     @user = users(:bob)
   end
 
-  test "layout links" do
+  test "layout links with user that is not logged in" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", signup_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", "http://news.railstutorial.org/"
+    assert_select "a[href=?]", users_path,            count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
+    assert_select "a[href=?]", user_path(@user),      count: 0
+    assert_select "a[href=?]", logout_path,           count: 0
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign Up")
+    get login_path
+    assert_select "title", full_title("Log in")
+    log_in_as(@user)
+    get edit_user_path @user
+  end
+
+    test "layout links with user that is logged in" do
+    log_in_as @user
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", signup_path
-    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", "http://news.railstutorial.org/"
+    assert_select "a[href=?]", users_path,            count: 1
+    assert_select "a[href=?]", edit_user_path(@user), count: 1
+    assert_select "a[href=?]", user_path(@user),      count: 1
+    assert_select "a[href=?]", logout_path,           count: 1
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
@@ -25,4 +55,32 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get edit_user_path @user
     assert_select "title", full_title("Edit #{@user.name}")
   end
+
+  test "titels with user that is not logged in" do
+    get root_path
+    assert_select "title", full_title
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign Up")
+    get login_path
+    assert_select "title", full_title("Log in")
+
+    get edit_user_path @user
+    follow_redirect!
+    assert_select "title", full_title("Log in")
+  end
+
+  test "titels with user that is logged in" do
+    log_in_as @user
+    get edit_user_path @user
+    assert_select "title", full_title("Edit #{@user.name}")
+    get user_path @user
+    assert_select "title", full_title(@user.name)
+    get users_path
+    assert_select "title", full_title("All users")
+
+  end
+
+
 end
