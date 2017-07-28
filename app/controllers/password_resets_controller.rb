da5611fcb9535e516014dd16680c7fd2221040c1
fabian <stahlschiff@web.de>
2017-07-28 10:40:57 +0200

    save time when user resets it's password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -35,8 +35,9 @@ class PasswordResetsController < ApplicationController
       password = params[:password_reset][:password]
       password_confirmation = params[:password_reset][:password_confirmation]
 
-      if @user.update_attributes({ password: password,
-                                  password_confirmation: password_confirmation})
+      if @user.update_attributes({  password: password,
+                                    password_confirmation: password_confirmation,
+                                    password_reset_at: Time.zone.now })
         flash[:success] = "Changes successfully saved."
         log_in @user
         redirect_to @user
