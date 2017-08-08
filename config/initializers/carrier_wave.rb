3aa98a868606afb67933e7de89b7ace834876327
fabian <stahlschiff@web.de>
2017-08-08 12:19:05 +0200

    add s3 region to fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,8 @@ if Rails.env.production?
       # configuration for Amazon S3
       provider:               'AWS',
       aws_access_key_id:      ENV['S3_ACCESS_KEY'] || 'NOT_SET',
-      aws_secret_access_key:  ENV['S3_SECRET_KEY'] || 'NOT_SET'
+      aws_secret_access_key:  ENV['S3_SECRET_KEY'] || 'NOT_SET',
+      region:                 ENV['S3_REGION'] || 'NOT_SET'
     }
     config.fog_directory =    ENV['S3_BUCKET'] || 'NOT_SET'
   end
