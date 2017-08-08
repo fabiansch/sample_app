e0651996fb01155b543c841decfe912a3de6c9d0
fabian <stahlschiff@web.de>
2017-08-08 23:50:50 +0200

    finish user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -96,4 +96,22 @@ class UserTest < ActiveSupport::TestCase
     bob.unfollow ana
     assert_not bob.following? ana
   end
+
+  test "feed should have the right posts" do
+    michael = users :michael
+    archer = users :archer
+    lana = users :lana
+    # posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include? post_following
+    end
+    # posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
